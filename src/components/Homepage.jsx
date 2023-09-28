fa45fdf460ab1c54996b8fff049eb7221842a4a4
Junaid Syed <sjunaid626@gmail.com>
2023-09-28 22:08:46 +0530

    Include bg image

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
 const Homepage = () => (
-  <div>Homepage</div>
+  <div className={`bg-home-background-mobile bg-cover bg-center h-screen relative flex flex-col opacity-75  ${'md:bg-home-background'}`} style={{ backgroundColor: 'rgba(0, 0, 0, 0.6)' }}>
+    Homepage
+  </div>
 );
 
 export default Homepage;
