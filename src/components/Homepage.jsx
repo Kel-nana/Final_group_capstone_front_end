11229d6a7aa374d0d94ba09089454efae501735a
Junaid Syed <sjunaid626@gmail.com>
2023-09-29 12:23:30 +0530

    Remove old files

--- src/components/Homepage.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Homepage = () => (
-  <div className={`bg-home-background-mobile bg-cover bg-center h-screen relative flex flex-col opacity-75  ${'md:bg-home-background'}`} style={{ backgroundColor: 'rgba(0, 0, 0, 0.6)' }}>
-    Homepage
-  </div>
-);
-
-export default Homepage;
