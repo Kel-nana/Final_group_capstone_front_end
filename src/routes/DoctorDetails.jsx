01b42df8a3f559f88c7df908301752e95bf0835b
Junaid Syed <sjunaid626@gmail.com>
2023-10-07 14:17:19 +0530

    Remove the gear icon

--- src/routes/DoctorDetails.jsx
+++ src/routes/DoctorDetails.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { useParams, Link } from 'react-router-dom';
-import { IoSettingsOutline } from 'react-icons/io5';
 import { IoIosArrowDropright, IoIosArrowDropleft } from 'react-icons/io';
 import Sidebar from './Sidebar';
 import '../assets/styles/doctordetails.css';
@@ -21,7 +20,7 @@ const DoctorDetails = () => {
           <img className="rounded-full w-80 h-80" src={doctor.profile_pic} alt="Doctor Profile" />
         </div>
 
-        <div className="flex flex-col mt-4">
+        <div className="flex flex-col mt-4 sm:items-end">
 
           <h3 className="text-3xl font-semibold uppercase tracking-wide name-styling sm:text-right">{doctor.doc_name}</h3>
           <h3 className="mb-2 text-sm font-semibold sm:text-right">{doctor.bio}</h3>
@@ -34,18 +33,21 @@ const DoctorDetails = () => {
             <p>Years of Experience</p>
             <h3>{doctor.years_of_experience}</h3>
           </div>
-          <button
-            type="button"
-            className="flex flex-row items-center justify-evenly mt-8 rounded-full bg-green-400 py-4 px-8 cursor-pointer hover:bg-green-500 transition-bg w-64 mx-auto sm:w-full"
-          >
-            <IoSettingsOutline className="text-3xl text-white" />
-            <Link
-              to="/new-appointment"
+
+          <div className="float-right">
+            <button
+              type="button"
+              className="flex flex-row items-center justify-evenly mt-8 rounded-full bg-[#97bf0f] hover:bg-[#5b740a] py-4 px-8 cursor-pointer transition-bg w-64 mx-auto"
             >
-              <span className="px-4 text-white">Get Appointment</span>
-            </Link>
-            <IoIosArrowDropright className="text-3xl text-white" />
-          </button>
+              <Link
+                to="/new-appointment"
+              >
+                <span className="px-4 text-white">Get Appointment</span>
+              </Link>
+              <IoIosArrowDropright className="text-3xl text-white" />
+            </button>
+          </div>
+
         </div>
       </div>
       <div>
@@ -53,7 +55,7 @@ const DoctorDetails = () => {
         <Link to="/doctors">
           <button
             type="button"
-            className="back-btn flex flex-row items-center justify-evenly mt-8 ml-70 rounded-r-full  bg-green-400 py-4 px-8 cursor-pointer hover:bg-green-500 transition-bg"
+            className="back-btn flex flex-row items-center justify-evenly mt-8 ml-70 rounded-r-full  bg-[#97bf0f] hover:bg-[#5b740a] py-4 px-8 cursor-pointer transition-bg"
           >
             <IoIosArrowDropleft className="text-3xl text-white" />
           </button>
