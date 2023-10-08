bedcfaa74e392053bf17270771470bf6ea54bf6f
Nahid1911 <nahid.raihan@gmail.com>
2023-10-08 06:23:05 +0600

    Add logout functionality in sidebar

--- src/routes/Sidebar.jsx
+++ src/routes/Sidebar.jsx
@@ -1,40 +1,47 @@
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import {
-  FaTwitter, FaFacebookF, FaGooglePlusG, FaVimeoV, FaPinterestP,
-} from 'react-icons/fa';
-import { HiMenuAlt4 } from 'react-icons/hi';
-import { AiOutlineCloseCircle } from 'react-icons/ai';
-import DocLogo from '../assets/logo.png';
+  FaTwitter,
+  FaFacebookF,
+  FaGooglePlusG,
+  FaVimeoV,
+  FaPinterestP,
+} from "react-icons/fa";
+import { HiMenuAlt4 } from "react-icons/hi";
+import { AiOutlineCloseCircle } from "react-icons/ai";
+import DocLogo from "../assets/logo.png";
 
 const socialIcons = [
-  FaTwitter, FaFacebookF, FaGooglePlusG, FaVimeoV, FaPinterestP,
+  FaTwitter,
+  FaFacebookF,
+  FaGooglePlusG,
+  FaVimeoV,
+  FaPinterestP,
 ];
 
 const Sidebar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const navigate = useNavigate();
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
-  const menuItems = [
-    { label: 'HOME', to: '/' },
-    { label: 'SIGN OUT', to: '/login' },
-  ];
+  const menuItems = [{ label: "HOME", to: "/" }];
 
-  const [activeBounce, setActiveBounce] = useState('');
+  const [activeBounce, setActiveBounce] = useState("");
   const [activeLink, setActivelink] = useState(null);
+  const [showModal, setShowModal] = useState(false);
 
   const BounceEffect = () => {
     setActiveBounce(true);
-    setActivelink('/');
+    setActivelink("/");
   };
 
   useEffect(() => {
     if (activeLink) {
       const timeoutId = setTimeout(() => {
-        setActivelink('/');
+        setActivelink("/");
       }, 700); // animation duration
 
       return () => {
@@ -55,6 +62,20 @@ const Sidebar = () => {
     }
   }, [activeBounce]);
 
+  const handleLogout = () => {
+    setShowModal(true);
+  };
+
+  const confirmLogout = () => {
+    localStorage.removeItem("token");
+    navigate("/");
+    setShowModal(false);
+  };
+
+  const cancelLogout = () => {
+    setShowModal(false);
+  };
+
   return (
     <>
       <div className="lg:hidden p-4">
@@ -66,13 +87,22 @@ const Sidebar = () => {
           <HiMenuAlt4 />
         </button>
       </div>
-      <div className={`fixed left-0 top-0 w-full h-full border-r bg-black z-10 opacity-65 border-r-gray-900 text-white transition-transform ease-in-out duration-500 ${isMenuOpen ? 'translate-x-0' : '-translate-x-full'}`}>
+      <div
+        className={`fixed left-0 top-0 w-full h-full border-r bg-black z-10 opacity-65 border-r-gray-900 text-white transition-transform ease-in-out duration-500 ${
+          isMenuOpen ? "translate-x-0" : "-translate-x-full"
+        }`}
+      >
         <ul className="pt-24 uppercase">
           <li className="py-4 px-8 cursor-pointer hover:text-green-400 transition-colors">
             <AiOutlineCloseCircle onClick={toggleMenu} className="text-3xl" />
           </li>
           {menuItems.map((item, index) => (
-            <li key={index} className={`py-4 px-8 ${index < menuItems.length - 1 ? 'border-b border-white-700' : ''} cursor-pointer hover:text-green-400 transition-colors`}>
+            <li
+              key={1}
+              className={`py-4 px-8 ${
+                index < menuItems.length - 1 ? "border-b border-white-700" : ""
+              } cursor-pointer hover:text-green-400 transition-colors`}
+            >
               <Link to={item.to} className="block w-full">
                 {item.label}
               </Link>
@@ -88,22 +118,39 @@ const Sidebar = () => {
         </ul>
       </div>
       <div className="text-[#181818] w-[20%] min-h-screen py-2 border-r-2 border-r-[#f3f3f3] overflow-x-hidden hidden lg:block">
-        <div className={`logo w-[15%] h-[9%] ml-[1%] mt-[1%] align-center justify-center ${activeBounce ? 'bounce' : ''}`} onClick={BounceEffect}>
-          <Link
-            to={activeLink}
-            className="block w-full "
-          >
+        <div
+          className={`logo w-[15%] h-[9%] ml-[1%] mt-[1%] align-center justify-center ${
+            activeBounce ? "bounce" : ""
+          }`}
+          onClick={BounceEffect}
+        >
+          <Link to={activeLink} className="block w-full ">
             <img src={DocLogo} alt="Logo Image" className="" />
           </Link>
         </div>
         <ul className="flex flex-col py-16 justify-center items-center text-base sm:text-lg md:text-lg lg:text-xl xl:text-2xl">
           {menuItems.map((item, index) => (
-            <li key={index} className="w-full text-center py-4 mb-4 hover:text-white hover:bg-green-400 transition-all">
+            <li
+              key={index}
+              className="w-full text-center py-4 mb-4 hover:text-white hover:bg-green-400 transition-all"
+            >
               <Link to={item.to} className="block w-full">
                 {item.label}
               </Link>
             </li>
           ))}
+          <li
+            key={1}
+            className="w-full text-center py-4 mb-4 hover:text-white hover:bg-green-400 transition-all"
+          >
+            <button
+              type="button"
+              onClick={handleLogout}
+              className="block w-full"
+            >
+              Sign out
+            </button>
+          </li>
         </ul>
         <ul className="flex py-24 flex-row self-end align-center justify-center">
           {socialIcons.map((Icon, index) => (
@@ -113,6 +160,29 @@ const Sidebar = () => {
           ))}
         </ul>
       </div>
+      {showModal && (
+        <div className="fixed inset-0 flex items-center justify-center z-50">
+          <div className="bg-green-300 p-6 rounded shadow-lg">
+            <h2 className="text-2xl font-semibold mb-4">Confirm Logout</h2>
+            <p>Are you sure you want to log out?</p>
+            <div className="mt-4">
+              <button
+                type="button"
+                onClick={confirmLogout}
+                className="bg-green-400 text-white px-4 py-2 rounded mr-2"
+              >
+                Logout
+              </button>
+              <button
+                className="bg-red-400 text-white px-4 py-2 rounded"
+                onClick={cancelLogout}
+              >
+                Cancel
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
     </>
   );
 };
