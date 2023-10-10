e1e71610505d204dbf59f1d3bac1ca54e3a8abbc
husush23 <husseinkadare40@gmail.com>
2023-10-10 18:41:30 +0300

    route for new doctor

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import './App.css';
-import { Routes, Route } from 'react-router-dom';
-import Homepage from './routes/Homepage';
-import Login from './routes/Login';
-import SignUp from './routes/SignUp';
-import Doctors from './routes/Doctors';
-import Appointments from './routes/Appointments';
-import DoctorDetail from './routes/DoctorDetails';
-import PrivateRoute from './routes/PrivateRoutes';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Homepage from "./routes/Homepage";
+import Login from "./routes/Login";
+import SignUp from "./routes/SignUp";
+import Doctors from "./routes/Doctors";
+import Appointments from "./routes/Appointments";
+import DoctorDetail from "./routes/DoctorDetails";
+import PrivateRoute from "./routes/PrivateRoutes";
+import AddDoctorForm from "./routes/AddDoctor";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Homepage />} />
+        <Route path="/new_doctor" element={<AddDoctorForm />} />
         <Route path="/login" element={<Login />} />
         <Route path="/sign_up" element={<SignUp />} />
         <Route
