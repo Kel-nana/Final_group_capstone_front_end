55080bd910b91fae3c975e4fde82671d3e1ab565
husush23 <husseinkadare40@gmail.com>
2023-10-10 14:32:40 +0300

    Routes updated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import { useState, useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Homepage from './routes/Homepage';
 import Login from './routes/Login';
@@ -7,29 +6,27 @@ import SignUp from './routes/SignUp';
 import Doctors from './routes/Doctors';
 import Appointments from './routes/Appointments';
 import DoctorDetail from './routes/DoctorDetails';
+import PrivateRoute from './routes/PrivateRoutes';
 
 function App() {
-  const [isLoggedIn, setLoggedIn] = useState(false);
-
-  useEffect(() => {
-    const token = localStorage.getItem('token');
-    if (token) {
-      setLoggedIn(true);
-    } else {
-      setLoggedIn(false);
-    }
-  }, []);
-
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/sign_up" element={<SignUp />} />
-        <Route path="/doctors" element={<Doctors />} />
-        <Route path="/appointments" element={<Appointments />} />
-        <Route path="/doctor/:id" element={<DoctorDetail />} />
-
+        <Route
+          path="/appointments"
+          element={<PrivateRoute element={<Appointments />} />}
+        />
+        <Route
+          path="/doctor/:id"
+          element={<PrivateRoute element={<DoctorDetail />} />}
+        />
+        <Route
+          path="/doctors"
+          element={<PrivateRoute element={<Doctors />} />}
+        />
       </Routes>
     </div>
   );
