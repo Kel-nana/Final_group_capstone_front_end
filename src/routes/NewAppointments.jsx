308e81ba794dff5b98906c91350f7408bbdb9102
kelvinirungu <kelvinn.irungu69@gmail.com>
2023-10-11 01:58:57 +0300

    Additional styling for form

--- src/routes/NewAppointments.jsx
+++ src/routes/NewAppointments.jsx
@@ -67,11 +67,11 @@ const NewAppointment = () => {
   return (
     <div className="new-appointment-container">
       <NewAppointmentMenu />
-      <h1 className="font-mono text-4xl font-black text-white pt-[15%] tracking-widest tracking-widest">BOOK APPOINTMENT</h1>
+      <h1 className="font-mono text-4xl font-black text-white pt-[12%] tracking-widest tracking-widest">BOOK APPOINTMENT</h1>
       <hr className="my-12 h-0.5 border-t-0 bg-neutral-100 opacity-100 dark:opacity-50 text-white w-[50%] ml-[25%]" />
       <p className="text-white "> Welcome to our hospital&apos;s appointment booking page! Your health is our priority, and we are here to make scheduling your visit as easy as possible. Whether you need a routine check-up, a specialist consultation, or any medical service, we&apos;ve got you covered. Our dedicated team of healthcare professionals is ready to assist you. Choose a convenient date and time for your appointment, and we&apos;ll ensure you receive the best care. Please provide your contact information, insurance details, and a brief description of your medical concern. Your well-being is our mission, and we look forward to serving you. Book your appointment now and take the first step toward a healthier, happier you</p>
 
-      <form onSubmit={handleSubmit} className="content-evenly flex flex-row w-[80vw] content-between space-x-1.5">
+      <form onSubmit={handleSubmit} className="content-evenly flex flex-row w-[80vw] content-between space-x-1.5 mt-[8vh] ">
         <div className="ml-[15%]">
           <DoctorDropdown
             allDoctorList={allDoctorList}
@@ -83,8 +83,8 @@ const NewAppointment = () => {
             .filter((doctor) => doctor.id === selectedDoctorIndex)
             .map((doctor) => (
               <div key={doctor.id}>
-                <p>
-                  Doctor&apos;s Location:
+                <p className="w-[16.2vw] h-[6.8vh] rounded-3xl  border-gray-300 border-2 animate-pulse justify-center mt-[3vh] ml-[0.8vw] text-white font-black items-center">
+                  Location:
                   {' '}
 
                   {' '}
@@ -98,7 +98,7 @@ const NewAppointment = () => {
           <input
             type="date"
             id="date"
-            className="rounded-3xl py-[7px] px-[15px]"
+            className="rounded-3xl py-[7px] px-[35px]"
             value={date}
             onChange={(e) => setDate(e.target.value)}
           />
@@ -108,7 +108,7 @@ const NewAppointment = () => {
           <input
             type="time"
             id="time"
-            className="rounded-3xl py-[7px] px-[15px] px-[15px] border-gray-300 outline-4"
+            className="rounded-3xl py-[7px] px-[55px] px-[15px] border-gray-300 outline-4"
             value={time}
             onChange={(e) => setTime(e.target.value)}
           />
