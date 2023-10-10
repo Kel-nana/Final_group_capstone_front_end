e626d935a6e73380e5d56a22caba291743de87f6
kelvinirungu <kelvinn.irungu69@gmail.com>
2023-10-11 00:24:17 +0300

    Additional styling for new appointment page

--- src/routes/NewAppointments.jsx
+++ src/routes/NewAppointments.jsx
@@ -71,43 +71,48 @@ const NewAppointment = () => {
       <hr className="my-12 h-0.5 border-t-0 bg-neutral-100 opacity-100 dark:opacity-50 text-white w-[50%] ml-[25%]" />
       <p className="text-white "> Welcome to our hospital&apos;s appointment booking page! Your health is our priority, and we are here to make scheduling your visit as easy as possible. Whether you need a routine check-up, a specialist consultation, or any medical service, we&apos;ve got you covered. Our dedicated team of healthcare professionals is ready to assist you. Choose a convenient date and time for your appointment, and we&apos;ll ensure you receive the best care. Please provide your contact information, insurance details, and a brief description of your medical concern. Your well-being is our mission, and we look forward to serving you. Book your appointment now and take the first step toward a healthier, happier you</p>
 
-      <form onSubmit={handleSubmit} className="" >
-        <DoctorDropdown
-          allDoctorList={allDoctorList}
-          onChange={handleDoctorChange}
-          selectedDoctorIndex={selectedDoctorIndex}
-          id="name"
-        />
-        {allDoctorList
-          .filter((doctor) => doctor.id === selectedDoctorIndex)
-          .map((doctor) => (
-            <div key={doctor.id}>
-              <p>
-                Doctor&apos;s Location:
-                {' '}
+      <form onSubmit={handleSubmit} className="content-evenly flex flex-row w-[80vw] content-between space-x-1.5">
+        <div className="ml-[15%]">
+          <DoctorDropdown
+            allDoctorList={allDoctorList}
+            onChange={handleDoctorChange}
+            selectedDoctorIndex={selectedDoctorIndex}
+            id="name"
+          />
+          {allDoctorList
+            .filter((doctor) => doctor.id === selectedDoctorIndex)
+            .map((doctor) => (
+              <div key={doctor.id}>
+                <p>
+                  Doctor&apos;s Location:
+                  {' '}
 
-                {' '}
-                {doctor.location ? doctor.location : 'Location'}
+                  {' '}
+                  {doctor.location ? doctor.location : 'Location'}
+                </p>
+              </div>
+            ))}
+        </div>
+        <div className="pl-[5%]">
+          <label htmlFor="date">Date</label>
+          <input
+            type="date"
+            id="date"
+            value={date}
+            onChange={(e) => setDate(e.target.value)}
+          />
+        </div>
+        <div className="pl-[5%]">
+          <label htmlFor="time">Time</label>
+          <input
+            type="time"
+            id="time"
+            value={time}
+            onChange={(e) => setTime(e.target.value)}
+          />
+        </div>
 
-              </p>
-            </div>
-          ))}
-        <label htmlFor="date">Date</label>
-        <input
-          type="date"
-          id="date"
-          value={date}
-          onChange={(e) => setDate(e.target.value)}
-        />
-        <label htmlFor="time">Time</label>
-        <input
-          type="time"
-          id="time"
-          value={time}
-          onChange={(e) => setTime(e.target.value)}
-        />
-
-        <button type="submit" onClick={handleSubmit}>Add Appointment</button>
+        <button type="submit" onClick={handleSubmit} className="pl-[5%]">Add Appointment</button>
       </form>
     </div>
   );
