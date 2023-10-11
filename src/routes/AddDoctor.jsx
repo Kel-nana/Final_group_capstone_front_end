f6c10f73cbc7a1ad29eecc83e55a810282a60943
Junaid Syed <sjunaid626@gmail.com>
2023-10-11 14:25:36 +0530

    Add styling on the AddDoctor page

--- src/routes/AddDoctor.jsx
+++ src/routes/AddDoctor.jsx
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 const AddDoctorForm = () => {
+  const navigate = useNavigate();
   const [doctorData, setDoctorData] = useState({
     doc_name: '',
     education: '',
@@ -59,6 +61,8 @@ const AddDoctorForm = () => {
 
       setSuccessMessage('Doctor added successfully');
 
+      navigate('/doctors');
+
       setDoctorData({
         doc_name: '',
         education: '',
@@ -73,74 +77,98 @@ const AddDoctorForm = () => {
   };
 
   return (
-    <div>
-      <h2>Add New Doctor</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="doc_name">Doctor Name</label>
-          <input
-            type="text"
-            name="doc_name"
-            value={doctorData.doc_name}
-            onChange={handleInputChange}
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor="education">Education</label>
-          <input
-            type="text"
-            name="education"
-            value={doctorData.education}
-            onChange={handleInputChange}
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor="profile_pic">Profile picture</label>
-          <input
-            type="text"
-            name="profile_pic"
-            onChange={handleInputChange}
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor="location">Location</label>
-          <input
-            type="text"
-            name="location"
-            value={doctorData.location}
-            onChange={handleInputChange}
-            required
-          />
-        </div>
+    <div className="bg-slate-100 mx-auto flex flex-col justify-center items-center min-h-screen">
+      <div className="flex flex-col justify-center items-center bg-white rounded shadow-md text-black px-4 py-8 mx-auto my-4 w-[90%] sm:w-[30%]">
+        <h2 className="mb-8 text-3xl text-center">Add New Doctor</h2>
+        <form onSubmit={handleSubmit} className="w-full">
+          <div>
+            <input
+              className="block focus:outline-none border border-grey-light w-full p-3 rounded mb-4"
+              placeholder="Name"
+              type="text"
+              name="doc_name"
+              value={doctorData.doc_name}
+              onChange={handleInputChange}
+              required
+            />
+          </div>
+          <div>
+            <input
+              className="block focus:outline-none border border-grey-light w-full p-3 rounded mb-4"
+              placeholder="Education"
+              type="text"
+              name="education"
+              value={doctorData.education}
+              onChange={handleInputChange}
+              required
+            />
+          </div>
+          <div>
+            <input
+              placeholder="Profile Picture URL"
+              className="Profile picture block focus:outline-none border border-grey-light w-full p-3 rounded mb-4"
+              type="text"
+              name="profile_pic"
+              onChange={handleInputChange}
+              required
+            />
+          </div>
+          <div>
+            <input
+              className="block focus:outline-none border border-grey-light w-full p-3 rounded mb-4"
+              placeholder="Location"
+              type="text"
+              name="location"
+              value={doctorData.location}
+              onChange={handleInputChange}
+              required
+            />
+          </div>
 
+          <div>
+            <textarea
+              className="block focus:outline-none border border-grey-light w-full p-3 rounded mb-4"
+              placeholder="Bio"
+              name="bio"
+              value={doctorData.bio}
+              onChange={handleInputChange}
+              required
+            />
+          </div>
+          <div>
+            <input
+              style={{
+                WebkitAppearance: 'textfield',
+                MozAppearance: 'textfield',
+                appearance: 'textfield',
+              }}
+              className="block focus:outline-none border border-grey-light w-full p-3 rounded mb-4"
+              placeholder="Years of Experience"
+              type="number"
+              name="years_of_experience"
+              value={doctorData.years_of_experience}
+              onChange={handleInputChange}
+              required
+            />
+          </div>
+          <div>
+            <button type="submit" className="w-full text-center py-3 rounded bg-[#97bf0f] hover:bg-[#5b740a] text-white py-2 px-4">Add Doctor</button>
+          </div>
+        </form>
+        {successMessage && <p>{successMessage}</p>}
+        {errorMessage && <p>{errorMessage}</p>}
         <div>
-          <label htmlFor="bio">Bio</label>
-          <textarea
-            name="bio"
-            value={doctorData.bio}
-            onChange={handleInputChange}
-            required
-          />
+          See all
+          {' '}
+          <a
+            type="button"
+            className="text-blue-500 inline-block"
+            href="/doctors"
+          >
+            Doctors
+          </a>
         </div>
-        <div>
-          <label htmlFor="years_of_experience">Years of Experience</label>
-          <input
-            type="number"
-            name="years_of_experience"
-            value={doctorData.years_of_experience}
-            onChange={handleInputChange}
-            required
-          />
-        </div>
-        <div>
-          <button type="submit">Add Doctor</button>
-        </div>
-      </form>
-      {successMessage && <p>{successMessage}</p>}
-      {errorMessage && <p>{errorMessage}</p>}
+      </div>
     </div>
   );
 };
