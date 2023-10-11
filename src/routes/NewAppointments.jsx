f2489df84f6fafca52ab070a0d8a9bdd3d7701b9
kelvinirungu <kelvinn.irungu69@gmail.com>
2023-10-11 12:57:37 +0300

    Addition of mobile and tablet version

--- src/routes/NewAppointments.jsx
+++ src/routes/NewAppointments.jsx
@@ -40,7 +40,6 @@ const NewAppointment = () => {
     doctorsLocation = null;
   }
 
-  console.log(date, time, doctorsLocation, 'updates live')
   useEffect(() => {
     dispatch(doctorData());
   }, [dispatch]);
@@ -60,11 +59,11 @@ const NewAppointment = () => {
     <div className="new-appointment-container">
       <NewAppointmentMenu />
       <h1 className="font-mono text-4xl font-black text-white pt-[12%] tracking-widest tracking-widest book-appoint">BOOK APPOINTMENT</h1>
-      <hr className="my-12 h-0.5 border-t-0 bg-neutral-100 opacity-100 dark:opacity-50 text-white w-[50%] ml-[25%]" />
-      <p className="text-white "> Welcome to our hospital&apos;s appointment booking page! Your health is our priority, and we are here to make scheduling your visit as easy as possible. Whether you need a routine check-up, a specialist consultation, or any medical service, we&apos;ve got you covered. Our dedicated team of healthcare professionals is ready to assist you. Choose a convenient date and time for your appointment, and we&apos;ll ensure you receive the best care. Please provide your contact information, insurance details, and a brief description of your medical concern. Your well-being is our mission, and we look forward to serving you. Book your appointment now and take the first step toward a healthier, happier you</p>
+      <hr className="my-12 h-0.5 border-t-0 bg-neutral-100 opacity-100 dark:opacity-50 text-white w-[50%] ml-[25%] form-line" />
+      <p className="text-white paragraph-text"> Welcome to our hospital&apos;s appointment booking page! Your health is our priority, and we are here to make scheduling your visit as easy as possible. Whether you need a routine check-up, a specialist consultation, or any medical service, we&apos;ve got you covered. Our dedicated team of healthcare professionals is ready to assist you. Choose a convenient date and time for your appointment, and we&apos;ll ensure you receive the best care. Please provide your contact information, insurance details, and a brief description of your medical concern. Your well-being is our mission, and we look forward to serving you. Book your appointment now and take the first step toward a healthier, happier you</p>
 
       <form onSubmit={handleSubmit} className="content-evenly flex flex-row w-[80vw] content-between space-x-1.5 mt-[8vh] form-container">
-        <div className="ml-[15%]">
+        <div className="ml-[5.5%]">
           <DoctorDropdown
             allDoctorList={allDoctorList}
             onChange={handleDoctorChange}
@@ -89,7 +88,7 @@ const NewAppointment = () => {
           <input
             type="date"
             id="date"
-            className="rounded-3xl py-[7px] px-[35px] border-gray-300 border-2 form-background text-white font-bold input-field-form"
+            className="rounded-3xl py-[7px] px-[25px] border-gray-300 border-2 form-background text-white font-bold "
             value={date}
             onChange={(e) => setDate(e.target.value)}
           />
@@ -98,17 +97,18 @@ const NewAppointment = () => {
           <input
             type="time"
             id="time"
-            className="rounded-3xl py-[7px] px-[55px] px-[15px] border-gray-300 border-2 form-background text-white font-bold input-field-form"
+            className="rounded-3xl py-[7px] px-[45px] px-[15px] border-gray-300 border-2 form-background text-white font-bold "
             value={time}
             onChange={(e) => setTime(e.target.value)}
           />
         </div>
+        <div className="pl-[3vw]"><p className="w-[205px] h-[6.8vh] rounded-3xl  border-gray-300 border-2 justify-center text-white font-black items-center border-gray-300 border-2 pt-[2.5%] form-background">{doctorsLocation || 'Location' }</p></div>
         <div className="pl-[5%]">
-          <button type="submit" onClick={handleSubmit} className="w-[15vw] h-[6.8vh] rounded-3xl  border-gray-300 border-2 animate-pulse justify-center  bg-white form-text text-white font-bold input-field-form">Book Now </button>
+
+          <button type="submit" onClick={handleSubmit} className="w-[15vw] h-[6.8vh] rounded-3xl  border-gray-300 border-2 animate-pulse justify-center  bg-white form-text text-white font-bold input-new-btn">Book Now </button>
 
         </div>
       </form>
-      <div className="w-[16.2vw] h-[6.8vh] rounded-3xl  border-gray-300 border-2 animate-pulse justify-center mt-[4vh] ml-[43%] text-white font-black items-center border-gray-300 border-2 input-field-form">{doctorsLocation || 'Location' }</div>
     </div>
   );
 };
